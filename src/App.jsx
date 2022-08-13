7fdde6c6a2beb5d419da702ff541e9f24273ba68
evribsap <evribsaputra@gmail.com>
2022-08-13 18:37:56 +0700

    change app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div className="bg-primary w-full overflow-hidden">
-      
-    </div>
-  )
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <h1 style={{ color: '#fff' }}>Hello</h1>
+  </div>
+);
 
 export default App
