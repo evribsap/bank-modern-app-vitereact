48fd4978cb31ecb8b80751fa3ff8d86f9a4de21f
evribsap <evribsaputra@gmail.com>
2022-08-16 22:32:27 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from './style';
 
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonial, Clients, CTA, Footer } from './components';
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -22,7 +22,7 @@ const App = () => (
         <Business />
         <Billing />
         <CardDeal />
-        <Testimonial />
+        <Testimonials />
         <Clients />
         <CTA />
         <Footer />
