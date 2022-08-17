c0e467a462d2348996eed9e7501f9284a5fa644c
evribsap <evribsaputra@gmail.com>
2022-08-17 23:41:14 +0700

    update billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,10 +1,10 @@
 import { apple, bill, google } from "../assets"
 import styles, { layout } from '../style'
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+
+  </section>
+)
 
 export default Billing
